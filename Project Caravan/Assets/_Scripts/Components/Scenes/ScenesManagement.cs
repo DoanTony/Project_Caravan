f5eb7af784f329ab62aa9a32ea798c6babeec34b
Tony Doan <doantony90@gmail.com>
2019-01-27 01:45:49 -0500

    updated dialogues

--- Project Caravan/Assets/_Scripts/Components/Scenes/ScenesManagement.cs
+++ Project Caravan/Assets/_Scripts/Components/Scenes/ScenesManagement.cs
@@ -5,56 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ScenesManagement : MonoBehaviour
 {
-   public enum OutdoorScenes
-    {
-        Outdoor_1,
-        Outdoor_2,
-        Outdoor_3,
-        Outdoor_4
-    }
-
-    public OutdoorScenes currentOutdoorScene;
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            SetScene(OutdoorScenes.Outdoor_1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            SetScene(OutdoorScenes.Outdoor_2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            SetScene(OutdoorScenes.Outdoor_3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            SetScene(OutdoorScenes.Outdoor_4);
-        }
-    }
-
-    public void SetScene(OutdoorScenes _scene)
-    {
-        switch (_scene)
-        {
-            case OutdoorScenes.Outdoor_1:
-                LoadScene("Outdoor1");
-                break;
-            case OutdoorScenes.Outdoor_2:
-                LoadScene("Outdoor2");
-                break;
-            case OutdoorScenes.Outdoor_3:
-                LoadScene("Outdoor3");
-                break;
-            case OutdoorScenes.Outdoor_4:
-                LoadScene("Outdoor4");
-                break;
-        }
-    }
-
-    private void LoadScene(string _sceneName)
+    public void LoadScene(string _sceneName)
     {
         for (int i = 0; i < 4; i++)
         {
