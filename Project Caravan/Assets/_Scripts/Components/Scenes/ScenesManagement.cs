3694c316312efc2033647250d599482fe0d2c0ee
Tony Doan <doantony90@gmail.com>
2019-01-27 00:32:34 -0500

    Added Scene manipulation system

---
+++ Project Caravan/Assets/_Scripts/Components/Scenes/ScenesManagement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManagement : MonoBehaviour
+{
+   public enum OutdoorScenes
+    {
+        Outdoor_1,
+        Outdoor_2,
+        Outdoor_3,
+        Outdoor_4
+    }
+
+    public OutdoorScenes currentOutdoorScene;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetScene(OutdoorScenes.Outdoor_1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetScene(OutdoorScenes.Outdoor_2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetScene(OutdoorScenes.Outdoor_3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetScene(OutdoorScenes.Outdoor_4);
+        }
+    }
+
+    public void SetScene(OutdoorScenes _scene)
+    {
+        switch (_scene)
+        {
+            case OutdoorScenes.Outdoor_1:
+                LoadScene("Outdoor1");
+                break;
+            case OutdoorScenes.Outdoor_2:
+                LoadScene("Outdoor2");
+                break;
+            case OutdoorScenes.Outdoor_3:
+                LoadScene("Outdoor3");
+                break;
+            case OutdoorScenes.Outdoor_4:
+                LoadScene("Outdoor4");
+                break;
+        }
+    }
+
+    private void LoadScene(string _sceneName)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            string outdoorString = "Outdoor" + (i+1);
+            if (outdoorString == _sceneName)
+            {
+                if (!SceneManager.GetSceneByName(outdoorString).isLoaded)
+                {
+                    SceneManager.LoadSceneAsync(outdoorString, LoadSceneMode.Additive);
+                }
+            }
+            else
+            {
+                if (SceneManager.GetSceneByName(outdoorString).isLoaded)
+                {
+                    SceneManager.UnloadSceneAsync(outdoorString);
+                }
+            }
+        }
+    }
+
+
+}
